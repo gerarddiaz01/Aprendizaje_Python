d8e20292916ccd1685e47be4abcb21e0fb76f7ac
Gerard Diaz <gerarddiazgibert@gmail.com>
2025-06-22 23:07:29 +0200

    Modify a file

--- Condicionales/Ejercicios/2b/Código relacion_numeros1.py
+++ Condicionales/Ejercicios/2b/Código relacion_numeros1.py
@@ -4,15 +4,14 @@ y = float(input("Introduce el segundo numero: "))
 z = float(input("Introduce el tercer numero: "))
 
 # --- Comprobar si alguno de ellos es la suma de los otros dos
-# Si es cierto imprimimos True 
-if (x==y+z):
+# Si es cierto imprimimos True
+if x == y + z:
     print("El primero es la suma de los otros dos")
-elif (y==x+z):
+elif y == x + z:
     print("El segundo es la suma de los otros dos")
-elif (z==x+y):
+elif z == x + y:
     print("El tercero es la suma de los otros dos")
 
 # Si es falso imprimir False
 else:
     print("Ninguno es la suma de los otros dos numeros")
-
